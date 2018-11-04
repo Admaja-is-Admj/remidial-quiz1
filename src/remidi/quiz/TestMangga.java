5118d7e55d00d72739d068acd35df87c0ff4c8c8
Admaja-is-Admj <Admaja-is-Admj>
2018-11-04 21:02:06 +0700

    InsyaAllah fix

--- src/remidi/quiz/TestMangga.java
+++ src/remidi/quiz/TestMangga.java
@@ -10,5 +10,20 @@ package remidi.quiz;
  * @author user
  */
 public class TestMangga {
-    
+    public static void main(String[] args) {
+        Pembeli orang = new Pembeli("Angga");
+        Mangga mangga = new Mangga("Manalagi", 10000);
+        Transaksi uang = new Transaksi(1, 20, mangga, orang);
+        System.out.println(uang.tampil());
+        
+        Pembeli orang2 = new Pembeli("Aflah");
+        Mangga mangga2 = new Mangga("Podang", 5000);
+        Transaksi uang2 = new Transaksi(2, 10, mangga2, orang2);
+        System.out.println(uang2.tampil());
+        
+        Pembeli orang3 = new Pembeli("Aditya");
+        Mangga mangga3 = new Mangga("Gadung", 15000);
+        Transaksi uang3 = new Transaksi(3, 6, mangga3, orang3);
+        System.out.println(uang3.tampil());
+    }
 }
